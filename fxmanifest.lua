2da190b80baf0ff812fbd8df11e81f9ee93f101d
CallumMcKinnonSnell <callumsnell@live.com>
2021-11-09 09:47:45 +0000

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,50 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+ui_page 'html/form.html'
+
+files {
+	'html/form.html',
+	'html/css.css',
+	'html/health.png',
+	'html/armour.png',
+	'html/food.png',
+	'html/water.png',
+	'html/voice.png',
+	'html/script.js',
+	'html/jquery-3.4.1.min.js',
+}
+
+client_scripts{
+    'config.lua',
+    'client/main.lua',
+}
+
+
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
+
+client_script '@WaveShield/xDxDxDxDxD.lua'
